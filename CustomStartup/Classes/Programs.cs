cfc03f928a946cf0539b1e25afca37031a7cb64c
Guilherme Fontana <gfontana3@ucs.br>
2023-05-21 00:24:02 -0300

    Add project files.

---
+++ CustomStartup/Classes/Programs.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStartup.Classes {
+    public class Programs {
+        public IList<string> Close { get; set; }
+        public IList<string> Start { get; set; }
+    }
+}
---
+++ CustomStartup/Program.cs
@@ -0,0 +1,35 @@
+﻿using CustomStartup.Classes;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.IO;
+
+
+namespace CustomStartup {
+    internal class Program {
+        static void Main(string[] args) {
+            string jsonFile = $@"{Directory.GetCurrentDirectory()}\programs.json";
+
+            if (!File.Exists(jsonFile))
+                return;
+
+            string programsFile = File.ReadAllText(jsonFile);
+
+            Programs programs = JsonConvert.DeserializeObject<Programs>(programsFile);
+
+            foreach (string processesToClose in programs.Close) {
+                try {
+                    foreach (Process process in Process.GetProcessesByName(processesToClose))
+                        process.Kill();
+                }
+                catch { }
+            }
+
+            foreach (string processesToStart in programs.Start) {
+                try {
+                    Process.Start(processesToStart);
+                }
+                catch { }
+            }
+        }
+    }
+}
